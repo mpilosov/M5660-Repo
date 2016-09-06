45bec5eaffe87507585c39c4664a95f0e81a560a
Jing Hu <jing.hu@ucdenver.edu>
2016-09-06 16:48:47 -0600

    JingHu made a change

--- hello_world.py
+++ hello_world.py
@@ -6,15 +6,11 @@ print(str+" my name is Varis Carey")
 print '\t my name is Daniel Bielich and I'm a pusher and I'm standing still.'
 print '\t my name is Michael Pilosov (first!)'
 print '\t my name is Jordan Hall'
-print '\t My name is Megan Sorenson and I'm a pusher!'
-print '\t my name is Jing Hu'
-print '\t my name is Mitchell Matheny and Im a pusher and im standing still '
+print '\t My name is Megan Sorenson and I'm a pusher'
+print '\t my name is Jing Hu and I'm a pusher'
 print "\t my name is Yuanlong Wang and I'm a pusher."
-print '\t ny name is Elise Reed and I am a pusher.'
-=======
-print "\t my name is Yuanlong Wang and I'm a pusher."'
+print '\t my name is Mitchell Matheny and Im a pusher'  
 print '\t my name is Elise Reed'
->>>>>>> Stashed changes
 print '\t my name is Chau Nguyen'
 print "\t My name is Doug Cheney and I'm a pusher."
 print '\t my name is Alexa Desautels'
