04c7ed8d77dd0ab68f418064b8c604ebfdda94e4
EDShapiro <evanshapiro@Evans-MacBook-Pro-2.local>
2016-09-06 22:03:59 -0600

    Standing Still

--- hello_world.py
+++ hello_world.py
@@ -15,5 +15,6 @@ print '\t my name is Chau Nguyen'
 print "\t My name is Doug Cheney and I'm a pusher."
 print '\t my name is Alexa Desautels'
 print '\t my name is Natalia Villegas-Franco I'm a pusher and I'm standing still.'
+print(str+" my name is Evan Shapiro and I push-a-ton and I'm standing still.")
 print '\t My name is Shanon Yeung.'
-print(str+" my name is Evan Shapiro and I push-a-ton")
+
