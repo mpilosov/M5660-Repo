872021f5d0e611655e303efdcb6b2af4750d6710
Michael Pilosov <michael.pilosov@ucdenver.edu>
2016-12-11 20:30:28 -0700

    changed modified somehow.

--- Comp2/Modified_Newtons_Root.py
+++ Comp2/Modified_Newtons_Root.py
@@ -8,15 +8,15 @@ import numpy as np
 import pylab as plt
 
 
-initial_guess = -400.0            # choose an initial guess 
+initial_guess = 40.0            # choose an initial guess 
 tol = 1E-64                      # set tolerance for root
 err = 1                          # initialize error 
 
 
-mode = 'mod'                    # Choose mode. orig / mod / hybrid
+mode = 'hybrid'                    # Choose mode. orig / mod / hybrid
 
-choose_root = 10                 # choose a random root
-multiplicity = 2                # choose multiplicity of root at x = 0
+choose_root = 20                 # choose a random root
+multiplicity = 5                # choose multiplicity of root at x = 0
 
 class fun:
     def __init__(self, choose_root=np.random.rand(), multiplicity=2):
@@ -51,7 +51,7 @@ class fun:
         self.calls += 1
         if self.rate is not None and self.old_rate is not None and self.calls>5:
             # print self.rate/self.old_rate 
-            if self.rate/self.old_rate > 0.99:
+            if self.rate/self.old_rate > 1-1E-2:
                 self.m = np.floor(1.0/(1.0 - self.rate))
                 print 'Changing m to %d'%(self.m)
                 self.calls = 0 # reset counter to make sure we give modified newton's a chance first.
@@ -119,18 +119,18 @@ ratio=temp[1:]/temp[0:-1]
 ratio2=temp[1:]/temp[0:-1]**2
 
 
-print x_ratio
-print ratio
-# print ' '
+# print x_ratio 
+# print ratio
+# # print ' '
 # print x_ratio2
-print ratio2
+# print ratio2
 
 
 ax.plot( range(len(x_ratio)), x_ratio )
 # if mode != 'mod':
-ax.plot( range(len(ratio)-1), x_ratio2 ) # don't look at quadratic for regular newton's on repeated root.
+# ax.plot( range(len(ratio)-1), x_ratio2 ) # don't look at quadratic for regular newton's on repeated root.
 
-#ax.plot( range(len(x_vec)), np.abs(x_vec - choose_root) )
+ax.plot( range(len(x_vec)), np.abs(x_vec - choose_root) )
 plt.ylabel('Convergence Rate', fontsize=fsize)
 plt.xlabel('Iterations', fontsize=fsize)
 # plt.yscale('log')
